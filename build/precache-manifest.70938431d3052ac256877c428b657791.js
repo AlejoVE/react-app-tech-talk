540ecc758a30bdecdcae0c88893a6ccde97e61f9
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2021-03-26 12:10:44 +0100

    Update title

---
+++ build/precache-manifest.70938431d3052ac256877c428b657791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dcd2aeb62c17ff82a9f7da5f0b3a89d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e36790812ad888411f30",
+    "url": "/static/css/main.d3901462.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36790812ad888411f30",
+    "url": "/static/js/main.e0de6062.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
